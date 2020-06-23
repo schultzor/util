4ce0c3f720eae930c60a15be63bf80bf4390a7c8
Chris Schultz <cjs@clsdn.com>
2020-06-23 15:38:49 -0500

    rename

--- lambdalogs/main.go
+++ lambdalogs/main.go
@@ -1,4 +1,5 @@
 // find all the AWS Lambda function log events for a given function in the last N hours and print them to stdout
+// assumes that AWS credentials are provided in the environment or other external configuration
 package main
 
 import (
@@ -10,24 +11,24 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	cwlogs "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	logs "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 )
 
 const lambdaPrefix = "/aws/lambda/"
 
-var client *cwlogs.CloudWatchLogs
+var client *logs.CloudWatchLogs
 
 func handle(e error) {
 	if e != nil {
-		fmt.Fprintf(os.Stderr, "error: %v", e)
+		fmt.Fprintf(os.Stderr, "exiting on error: %v", e)
 		os.Exit(1)
 	}
 }
 
 func listGroups() {
-	err := client.DescribeLogGroupsPages(&cwlogs.DescribeLogGroupsInput{
+	err := client.DescribeLogGroupsPages(&logs.DescribeLogGroupsInput{
 		LogGroupNamePrefix: aws.String(lambdaPrefix),
-	}, func(page *cwlogs.DescribeLogGroupsOutput, lastPage bool) bool {
+	}, func(page *logs.DescribeLogGroupsOutput, lastPage bool) bool {
 		for _, x := range page.LogGroups {
 			fmt.Println(strings.TrimPrefix(*x.LogGroupName, lambdaPrefix))
 		}
@@ -46,7 +47,7 @@ func main() {
 	flag.Parse()
 	funcName := flag.Arg(0)
 
-	client = cwlogs.New(session.Must(session.NewSession(&aws.Config{Region: region})))
+	client = logs.New(session.Must(session.NewSession(&aws.Config{Region: region})))
 	if funcName == "" {
 		fmt.Fprintf(os.Stderr, "No function argument given, listing functions in %s:", *region)
 		listGroups()
@@ -56,10 +57,10 @@ func main() {
 	groupName := lambdaPrefix + funcName
 	fmt.Fprintf(os.Stderr, "Finding streams for %s with last event time after %s\n", groupName, cutoff.Format(time.RFC3339))
 	streams := []string{}
-	err := client.DescribeLogStreamsPages(&cwlogs.DescribeLogStreamsInput{
+	err := client.DescribeLogStreamsPages(&logs.DescribeLogStreamsInput{
 		Descending:   aws.Bool(false), // print oldest log streams first
 		LogGroupName: &groupName,
-	}, func(page *cwlogs.DescribeLogStreamsOutput, lastPage bool) bool {
+	}, func(page *logs.DescribeLogStreamsOutput, lastPage bool) bool {
 		for _, x := range page.LogStreams {
 			if x.LastEventTimestamp != nil && ParseMillis(*x.LastEventTimestamp).After(cutoff) {
 				if x.LogStreamName != nil {
@@ -72,11 +73,11 @@ func main() {
 	handle(err)
 	for _, streamName := range streams {
 		fmt.Fprintf(os.Stderr, "Getting log events in %s/%s\n", groupName, streamName)
-		err := client.GetLogEventsPages(&cwlogs.GetLogEventsInput{
+		err := client.GetLogEventsPages(&logs.GetLogEventsInput{
 			LogGroupName:  &groupName,
 			LogStreamName: &streamName,
 		},
-			func(page *cwlogs.GetLogEventsOutput, lastPage bool) bool {
+			func(page *logs.GetLogEventsOutput, lastPage bool) bool {
 				for _, e := range page.Events {
 					fmt.Printf("%v %s", *e.Timestamp, *e.Message)
 				}
